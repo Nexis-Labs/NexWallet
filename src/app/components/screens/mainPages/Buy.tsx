c49ba73232e132c80e591b1a0e360e49fb650404
Detonation <166897641+0xdetonation@users.noreply.github.com>
2024-07-08 17:11:45 +0530

    password btn

--- src/app/components/screens/mainPages/Buy.tsx
+++ src/app/components/screens/mainPages/Buy.tsx
@@ -110,6 +110,15 @@ function VoteAccountCard({ voteAcc, seedPhrase }: Props) {
   );
 }
 
+const BottomGradient = () => {
+  return (
+    <>
+      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-green-500 to-transparent" />
+      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+    </>
+  );
+};
+
 function Buy() {
   const [password, setPassword] = useState<string>("");
   const [seedPhrase, setSeedPhrase] = useState<string[]>([]);
@@ -386,10 +395,12 @@ function Buy() {
             type="password"
           />
           <button
+            className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset] mt-2"
+            type="submit"
             onClick={handleSubmit}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           >
-            Access Staking Page
+            Access Staking Page &rarr;
+            <BottomGradient />
           </button>
         </>
       )}
